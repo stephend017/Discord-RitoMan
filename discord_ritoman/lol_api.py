4ec248873357fcb301465f2892564af1647fd84f
Stephen Davis <davi1138@purdue.edu>
2020-11-18 05:53:34 -1000

    Updated lol_api

--- discord_ritoman/lol_api.py
+++ discord_ritoman/lol_api.py
@@ -1,22 +1,25 @@
 """
 this is the api for mypackage
 """
-from discord_ritoman.lol_match_data import LoLMatchData
-from discord_ritoman.db_api import get_all_discord_users
-from typing import List
+from typing import Any
 from discord_ritoman.lol_match_metadata import LoLMatchMetadata
 import requests
-import datetime
-from discord_ritoman.utils import unix_time_millis
 import os
 
 
 RIOT_TOKEN = os.getenv("RIOT_TOKEN", None)
 
 
-def get_account_id(puuid: str):
-    """"""
-    url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+def riot_api_get(url: str) -> Any:
+    """
+    Generic method for making a GET request to the riot API
+
+    Args:
+        url (str): the url endpoint of the request
+
+    Returns:
+        Any: the json object returned by a successful request
+    """
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9",
@@ -26,7 +29,7 @@ def get_account_id(puuid: str):
     }
     response = requests.get(url, headers=headers)
     if response.ok:
-        return response.json()["accountId"]
+        return response.json()
 
     print(
         f"GET encrypted account ID request failed: [{response.status_code}] {response.text}"
@@ -34,66 +37,66 @@ def get_account_id(puuid: str):
     raise Exception(
         f"GET encrypted account ID request failed: [{response.status_code}] {response.text}"
     )
+
+
+def get_account_id(puuid: str) -> str:
+    """
+    Returns the accountId for a given riot puuid
+
+    Note: the PUUID must be from the summoner API. The
+    current API token does not support the account API
+
+    Args:
+        puuid (str): the riot puuid (see riot developer docs)
+
+    Returns:
+        accoundId (str): the riot accountId associated with the
+            given PUUID
+    """
+    url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    return riot_api_get(url)["accountId"]
 
 
 def get_matches(account_id: str, start_timestamp: int):
-    """"""
+    """
+
+    """
     url = f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?beginTime={start_timestamp}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": RIOT_TOKEN,
-    }
-    response = requests.get(url, headers=headers)
-    if response.ok:
-        return [
-            LoLMatchMetadata(
-                item["gameId"], item["champion"], item["timestamp"]
-            )
-            for item in response.json()["matches"]
-        ]
-
-    print(
-        f"GET matches request failed: [{response.status_code}] {response.text}"
-    )
-    raise Exception(
-        f"GET matches request failed: [{response.status_code}] {response.text}"
-    )
+    response = riot_api_get(url)
+    return [
+        LoLMatchMetadata(item["gameId"], item["champion"], item["timestamp"])
+        for item in response.json()["matches"]
+    ]
 
 
-def get_match_data(match_id: int):
-    """"""
+def get_match_data(match_id: int) -> Any:
+    """
+    Returns all the match data for a given match
+
+    This returns information about the match, mainly
+    overall statistics, team compositions and state of
+    the match when it ended
+
+    Args:
+        match_id (int): the numeric identifier for a
+            match (see riot developer docs)
+
+    Returns:
+        Any: the full json response from the riot API
+    """
     url = f"https://na1.api.riotgames.com/lol/match/v4/matches/{match_id}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": RIOT_TOKEN,
-    }
-    response = requests.get(url, headers=headers)
-    if response.ok:
-        return response.json()
-    raise Exception(
-        f"GET match data request failed: [{response.status_code}] {response.text}"
-    )
+    return riot_api_get(url)
 
 
-def get_match_timeline(match_id: int):
-    """"""
+def get_match_timeline(match_id: int) -> Any:
+    """
+    Returns a timeline of the match
+
+    Args:
+        match_id (int): the numeric identifier for the match
+
+    Returns:
+        Any: the full json response from the riot API
+    """
     url = f"https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/{match_id}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": RIOT_TOKEN,
-    }
-    response = requests.get(url, headers=headers)
-    if response.ok:
-        return response.json()
-    raise Exception(
-        f"GET match timeline request failed: [{response.status_code}] {response.text}"
-    )
+    return riot_api_get(url)
