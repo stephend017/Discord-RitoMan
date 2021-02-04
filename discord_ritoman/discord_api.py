55d5e11da916fc1be77cae32bdc2eb9eb73f0ddd
Stephen Davis <davi1138@purdue.edu>
2021-02-04 15:56:14 -0500

    Formatting changes

--- discord_ritoman/discord_api.py
+++ discord_ritoman/discord_api.py
@@ -3,30 +3,7 @@ import requests
 
 from discord_ritoman.utils import create_logger
 
-# import logging
-# from logging.handlers import RotatingFileHandler
 
-# log_formatter = logging.Formatter(
-#     "%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s"
-# )
-
-# logFile = "./discord_api.log"
-
-# my_handler = RotatingFileHandler(
-#     logFile,
-#     mode="a",
-#     maxBytes=5 * 1024 * 1024,
-#     backupCount=2,
-#     encoding=None,
-#     delay=0,
-# )
-# my_handler.setFormatter(log_formatter)
-# my_handler.setLevel(logging.INFO)
-
-# logger = logging.getLogger("discord-api")
-# logger.setLevel(logging.INFO)
-
-# logger.addHandler(my_handler)
 logger = create_logger(__file__)
 
 
