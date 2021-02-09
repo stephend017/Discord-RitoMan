763219082be440b5a93d3d0db481b4ab7ac635f7
Stephen Davis <davi1138@purdue.edu>
2021-02-09 09:22:25 -0500

    Added new db accessors

---
+++ discord_ritoman/db/accessors.py
@@ -0,0 +1,131 @@
+"""
+This class is where all database accessor functions will be defined
+"""
+from typing import List
+
+from sqlalchemy.orm.exc import NoResultFound
+
+from discord_ritoman.models import GameResult
+from discord_ritoman.db.schema import LoLText, LoLTextGroup, LoLUser
+from discord_ritoman.db.session import session
+
+
+def get_all_lol_users() -> List[LoLUser]:
+    """
+    Returns a list of all LoLUsers
+
+    Returns:
+        List[LoLUser]: all lol users
+    """
+    return session.query(LoLUser).all()
+
+
+def get_lol_text_by_group(group_name: str) -> List[LoLText]:
+    """
+    Returns all text from a specific group
+
+    Args:
+        group_name (str): the name of the text group to query
+
+    Returns:
+        List[LoLText]: All the text that belongs to the given group
+    """
+    query_result = (
+        session.query(LoLTextGroup)
+        .filter(LoLTextGroup.name == group_name)
+        .all()
+    )
+    if len(query_result) != 1:
+        raise Exception(f"[ERROR]: LoLTextGroup ({group_name}) doesn't exist")
+
+    group: LoLTextGroup = query_result[0]
+
+    return session.query(LoLText).filter(LoLText.group == group.uuid).all()
+
+
+def update_lol_user_last_updated(user: LoLUser, timestamp: int):
+    """
+    Updates a users last updated time
+
+    Args:
+        user (LoLuser): the user to update
+        timestamp (int): the time in unix ms that the user
+            was updated at
+    """
+    user.last_updated = timestamp
+    session.commit()
+
+
+def update_lol_user_winrate(user: LoLUser, game_result: GameResult):
+    """
+    Updates a given users winrate
+
+    Note: currently only wins and losses are handled
+
+    Args:
+        user (LoLUser): the user to update
+        game_result (GameResult): the result of the game
+    """
+    if game_result == GameResult.WIN:
+        user.wins = LoLUser.wins + 1
+        session.commit()
+
+    if game_result == GameResult.LOSS:
+        user.losses = LoLUser.losses + 1
+        session.commit()
+
+
+def set_lol_user_winrate(user: LoLUser, value: bool):
+    """
+    Sets winrate tracking for a given user
+
+    Args:
+        user (LoLUser): the user to enable winrate tracking for
+        value (bool): True if winrate should be enabled, false otherwise
+    """
+    user.winrate = value
+    session.commit()
+
+
+def get_lol_users_with_winrate_enabled() -> List[LoLUser]:
+    """
+    Returns all users who currently have winrate enabled
+
+    Returns:
+        List[LoLUser]: any lol user who's winrate flag is
+            set to true
+    """
+    return session.query(LoLUser).filter(LoLUser.winrate).all()
+
+
+def create_new_lol_user(discord_id: int, riot_puuid: str):
+    """
+    Creates a new lol user with the given discord id and riot puuid
+
+    Args:
+        discord_id (int): the id of the discord user
+        riot_puuid (str): the id of the riot account
+    """
+    user = LoLUser(discord_id, riot_puuid)
+    session.add(user)
+    session.commit()
+
+
+def get_lol_user_by_discord_id(discord_id: int) -> LoLUser:
+    """
+    Returns the LoL user with the given discord id
+
+    Args:
+        discord_id (int): the discord id of the user to query
+
+    Returns
+        LoLUser: the lol user object with the given discord_id
+    """
+    try:
+        return (
+            session.query(LoLUser)
+            .filter(LoLUser.discord_id == discord_id)
+            .one()
+        )
+    except NoResultFound:
+        return None
---
+++ tests/test_db_accessors.py
@@ -0,0 +1,197 @@
+from discord_ritoman.models import GameResult
+import pytest
+
+
+from typing import List
+from discord_ritoman.db.accessors import (
+    create_new_lol_user,
+    get_all_lol_users,
+    get_lol_text_by_group,
+    get_lol_user_by_discord_id,
+    get_lol_users_with_winrate_enabled,
+    set_lol_user_winrate,
+    update_lol_user_last_updated,
+    update_lol_user_winrate,
+)
+from discord_ritoman.db.schema import LoLText, LoLTextGroup, LoLUser
+from discord_ritoman.db.session import session
+
+
+def mock_users() -> List[LoLUser]:
+    """
+    returns all mock users used by this module
+    """
+    return [LoLUser(1, "r1"), LoLUser(2, "r2", True), LoLUser(3, "r3", True)]
+
+
+def mock_text_groups() -> List[LoLTextGroup]:
+    """
+    Returns all mock text groups used by this module
+    """
+    return [
+        LoLTextGroup("prefix", "text before message"),
+        LoLTextGroup("suffix", "text after message"),
+    ]
+
+
+def mock_text(group1: LoLTextGroup, group2: LoLTextGroup) -> List[LoLText]:
+    """
+    Returns all mock text
+    """
+    return [
+        LoLText(group1.uuid, "testing 1"),
+        LoLText(group1.uuid, "2 testing"),
+        LoLText(group2.uuid, "another one"),
+    ]
+
+
+def setup_module(module):
+    """ setup any state specific to the execution of the given module."""
+    # clean db before insertions
+    session.query(LoLUser).delete()
+    session.commit()
+
+    # populate db with mock users to test with
+    users = mock_users()
+    for user in users:
+        session.add(user)
+        session.commit()
+
+    text_groups = mock_text_groups()
+    for text_group in text_groups:
+        session.add(text_group)
+        session.commit()
+
+    texts = mock_text(text_groups[0], text_groups[1])
+    for text in texts:
+        session.add(text)
+        session.commit()
+
+
+def teardown_module(module):
+    """teardown any state that was previously setup with a setup_module
+    method.
+    """
+    # remove mock users
+    session.query(LoLUser).delete()
+    session.commit()
+
+    # remove mock text
+    session.query(LoLText).delete()
+    session.commit()
+
+    # remove mock text_groups
+    session.query(LoLTextGroup).delete()
+    session.commit()
+
+
+def test_get_all_lol_users():
+    """
+    tests that get all lol users works correctly
+    """
+    expected: List[LoLUser] = mock_users()
+    users: List[LoLUser] = get_all_lol_users()
+    for user in users:
+        found = False
+        for e_user in expected:
+            if (
+                e_user.discord_id == user.discord_id
+                and e_user.riot_puuid == user.riot_puuid
+                and e_user.winrate == user.winrate
+            ):
+                found = True
+                break
+        assert found
+
+
+def test_get_lol_text_by_group():
+    """
+    Tests that text can be queried by group
+    """
+    texts = get_lol_text_by_group("prefix")
+    groups = mock_text_groups()
+    expected_texts = mock_text(groups[0], groups[1])[:-1]  # only use first 2
+    for expected in expected_texts:
+        found = False
+        for text in texts:
+            if expected.text == text.text:
+                found = True
+                break
+        assert found
+
+
+def test_update_lol_user_last_updated():
+    """
+    Tests that a user's last update time works correctly
+    """
+    user = mock_users()[0]
+    update_lol_user_last_updated(user, -10)
+    assert user.last_updated == -10
+
+
+def test_update_lol_user_winrate():
+    """
+    Tests that a user's winrate can be updated sucessfully
+    """
+    user = mock_users()[1]  # user with winrate enabled
+    assert user.wins == 0
+    assert user.losses == 0
+
+    update_lol_user_winrate(user, GameResult.WIN)
+    assert user.wins.compare(LoLUser.wins + 1)
+
+    update_lol_user_winrate(user, GameResult.LOSS)
+    assert user.losses.compare(LoLUser.losses + 1)
+
+    update_lol_user_winrate(user, GameResult.NONE)
+    assert user.wins.compare(LoLUser.wins + 1) and user.losses.compare(
+        LoLUser.losses + 1
+    )
+
+
+def test_set_lol_user_winrate():
+    """
+    Tests that setting a winrate state (true or false) works correctly
+    """
+    user = mock_users()[0]
+    assert not user.winrate
+
+    set_lol_user_winrate(user, True)
+    assert user.winrate
+
+    set_lol_user_winrate(user, False)
+    assert not user.winrate
+
+
+def test_get_lol_users_with_winrate_enabled():
+    """
+    Tests that getting a users with winrate enabled returns true
+    """
+
+    users = get_lol_users_with_winrate_enabled()
+    for user in users:
+        found = True
+        for e_user in mock_users()[1:]:
+            if e_user.discord_id == user.discord_id:
+                found = True
+                break
+        assert found
+
+
+def test_create_new_lol_user():
+    """
+    Tests that creating a new lol user works correctly
+    """
+    current = session.query(LoLUser).all()
+    assert len(current) == 3
+    create_new_lol_user(4, "r4")
+    assert len(session.query(LoLUser).all()) == 4
+
+
+def test_get_lol_user_by_discord_id():
+    """
+    Tests that getting a user by their given discord id
+    works correctly
+    """
+    assert get_lol_user_by_discord_id(1)
+    assert get_lol_user_by_discord_id(6) is None
