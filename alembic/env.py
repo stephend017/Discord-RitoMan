a41d0cc2795eba6ce77375b557016c37399ac629
Stephen Davis <davi1138@purdue.edu>
2021-02-09 09:52:41 -0500

    Commenting

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,6 @@ import os
 
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
 from sqlalchemy.engine import create_engine
 
