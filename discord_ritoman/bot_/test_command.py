fb48e350fc36b60967caefa28b1f2eb6c4e45b1b
Stephen Davis <davi1138@purdue.edu>
2021-02-18 20:48:48 -0500

    final try i swear (#81)

--- discord_ritoman/bot_/test_command.py
+++ discord_ritoman/bot_/test_command.py
@@ -3,7 +3,7 @@ from discord_ritoman.bot_.bot_command import bot_command
 
 
 @bot_command("testcommand")
-class MyBotCommand:
+class AnotherBotCommand:
     @staticmethod
     async def default(ctx, *args, **kwargs):
         await ctx.send("yes")
