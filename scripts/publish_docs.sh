ed6bb646ecac5ec20a2bb38709c7a5c87d8bd2c1
Stephen Davis <davi1138@purdue.edu>
2021-03-04 09:21:29 -0500

    Update publish_docs.sh

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -7,6 +7,7 @@ WIKI_FOLDER="wiki"
 WIKI_REPO="https://github.com/stephend017/discord_ritoman.wiki.git"
 
 GH_TOKEN=$1
+GH_ACTOR=$2
 
 # setup wiki repo
 if [[ ! -d "./$WIKI_FOLDER" ]]; then
@@ -14,6 +15,9 @@ if [[ ! -d "./$WIKI_FOLDER" ]]; then
 fi
 
 git submodule update "$WIKI_FOLDER"
+git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/discord_ritoman.wiki.git"
+git pull github ${GITHUB_REF} --ff-only
+
 env/bin/pydoc-markdown -I . -m "$DOC_SOURCE" '{
     renderer: {
       type: markdown,
@@ -29,8 +33,11 @@ env/bin/pydoc-markdown -I . -m "$DOC_SOURCE" '{
       }
     }
   }' > "./$WIKI_FOLDER/$DOC_NAME.md"
+  
 cd "$WIKI_FOLDER"
 git checkout master
 git add "./$DOC_NAME.md"
 git commit -m "Updated Documentation"
-git push "https://$GH_TOKEN@github.com/stephend017/discord_ritoman.wiki.git"
+
+git push github HEAD:${GITHUB_REF}
+# git push "https://$GH_TOKEN@github.com/stephend017/discord_ritoman.wiki.git"
