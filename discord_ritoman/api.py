049e108efd52eb4e661b96ce8351b664e97de16d
stephend017 <stephenedavis17@gmail.com>
2021-07-05 00:27:20 -0400

    spelling mistake

--- discord_ritoman/api.py
+++ discord_ritoman/api.py
@@ -136,10 +136,13 @@ def _poll_game_start():
             get_active_game,
             logger,
             f"Failed to get matches for user=[{user.discord_id}]",
-            [],
+            None,
             account_id=account_id,
             start_timestamp=user.last_updated,
         )
+
+        if game is None:
+            continue
 
         if (
             len(
