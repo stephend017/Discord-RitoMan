75f1aeac696d11cfd986d6ab740283df442eaed5
stephend017 <stephenedavis17@gmail.com>
2021-07-04 23:01:23 -0400

    changed poll timer to 30 seconds

--- discord_ritoman/__main__.py
+++ discord_ritoman/__main__.py
@@ -5,7 +5,7 @@ import time
 
 def main():
     """"""
-    schedule.every(5).minutes.do(poll_lol_api)
+    schedule.every(30).seconds.do(poll_lol_api)
 
     schedule.every().days.at("12:00:00").do(run_end_of_day)
 
